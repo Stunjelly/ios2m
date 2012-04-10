f0f32c4727d740f3a3038b28e8df0e8ee1a819a6
Nic West <nic@stunjelly.com>
2012-04-10 20:16:23 +0100

    -added recursive folder search -started the fix bit -disabled buttons while search/fix is in progress. -added a little bit of error handling in a couple of places.
    need to rework epub module as you can't open a zipFile for both reading
    and writing.

--- test.py
+++ test.py
@@ -32,69 +32,107 @@ class Main(QtGui.QMainWindow):
         self.ui.btnabout.clicked.connect(self.AboutDialog)
         self.ui.btnfix.clicked.connect(self.FixEPUBs)
         self.ui.treeWidget.setColumnWidth(0, 230)
-        self.warningcolor = QtGui.QColor("red")
+        self.warningcolor = QtGui.QColor("yellow")
         self.okcolor = QtGui.QColor("green")
+        self.errorcolor = QtGui.QColor("red")
+        self.filesToFix = []
         
         self.DirEpub = ""
         
     def FixEPUBs(self):
-        if not self.DirEpub == "":
-            fixedpath = os.path.join(self.DirEpub, "/Fixed")
-            os.mkdir(fixedpath)
-            print fixedpath
-            subprocess.Popen("explorer " + fixedpath)
+        self.ui.btnopen.setEnabled(False)
+        self.ui.btnabout.setEnabled(False)
+        self.ui.btnfix.setEnabled(False)
+        if len(self.filesToFix) > 0:
+            for path, treeItem in self.filesToFix:
+                test = epub.EpubFile(path)
+                for item in test.opf.manifest:
+                    if str.startswith(str(item.mimetype), "image"):
+                        fakefile = StringIO(item.read())
+                        img = Image.open(fakefile)
+                        if self.testImage(img):
+                            area = 1980000
+                            width, height = img.size
+                            resize = math.sqrt(area)/math.sqrt(width*height)
+                            width = math.floor(width*resize)
+                            height = math.floor(height*resize)
+                            img.resize((int(width), int(height)), Image.ANTIALIAS)
+                            #this is borked need to rework the epub module
+                            #item.write(img.tostring())
+                            print "writing "+item.archloc+" to "+path
+        else:
+            QtGui.QMessageBox.information(self
+            , "No Files To Fix"
+            , "Please select a folder first"
+            , QtGui.QMessageBox.Ok)
+        
+        self.ui.btnopen.setEnabled(True)
+        self.ui.btnabout.setEnabled(True)
+        self.ui.btnfix.setEnabled(True)
     
     def AboutDialog(self):
         QtGui.QMessageBox.about(self,
                           "About " + self.progname,
-                          self.progname + " " + self.version +"\n\nApple iBooks will reject any submitted EPUB to the iBookstore which doesn't meet it's criteria. This application was developed to fix EPUBs whose images pixel count exceed 2 million.\n\nIf you come across any bugs or issues with this software you can email us directly at mail@stunjelly.com\n\nCoded by Edward Knowles and Nic West of " + self.author + ".")    
+                          self.progname + " " + self.version +"\n\nApple iBooks will reject any submitted EPUB to the iBookstore which doesn't meet it's criteria. This application was developed to fix EPUBs whose images pixel count exceed 2 million.\n\nIf you come across any bugs or issues with this software you can email us directly at mail@stunjelly.com\n\nCoded by Edward Knowles and Nic West of " + self.author + ".")
         
+    def testImage(self, img):        
+        width, height = img.size
+        if width*height > 2000000:
+            return True
+        else:
+            return False
+         
     def AddEpubToList(self, epubsfolder):
+        self.ui.btnopen.setEnabled(False)
+        self.ui.btnabout.setEnabled(False)
+        self.ui.btnfix.setEnabled(False)
         TotalImagesFix = 0
         TotalImagesFound = 0
         EpubsAffected = 0
         FilesScanned = 0
         starttime = int(time.time())
-        dirList=os.listdir(epubsfolder)
-        for fname in dirList:
-            if fname.endswith("epub"):
-                FilesScanned = FilesScanned + 1
-                fullfile = os.path.join(str(epubsfolder), fname)
-                test = epub.EpubFile(fullfile)
-                images = 0
-                images_corrected = 0
+        for root, subFolders, files in os.walk(str(epubsfolder)):
+            for file in files:
+                fname = file
                 ebook = QtGui.QTreeWidgetItem()
-                ebook.setText(0, fname)
-                self.ui.statusbar.showMessage("Checking " + fname + "...")
-                print "Checking " + fname + "..."
-                for item in test.opf.manifest:
-                    if str.startswith(str(item.mimetype), "image") == True:
-                        images = images+1
-                        img = Image.open(StringIO(item.read()))
-                        width, height = img.size
-                        if width*height > 2000000:
-                            images_corrected = images_corrected+1
-                            print width, height, width*height
-                            area = 1900000
-                            resize = math.sqrt(area)/math.sqrt(width*height)
-                            print math.floor(width*resize), math.floor(height*resize), (width*resize)*(height*resize)
-                            print resize 
-                ebook.setText(2, str(images))
-                ebook.setText(3, str(images_corrected))
-                if images_corrected == 0:
-                    ebook.setText(1, "OK")
-                    ebook.setTextColor(1, self.okcolor)
-                else:
-                    EpubsAffected = EpubsAffected + 1
-                    ebook.setText(1, "Warnings")
-                    ebook.setTextColor(1, self.warningcolor)
-                epubsize = os.path.getsize(fullfile)
-                nicesize = self.prettySize(epubsize)
-                ebook.setText(4, nicesize)
-                TotalImagesFound = TotalImagesFound + images
-                TotalImagesFix = TotalImagesFix + images_corrected
-            self.ui.treeWidget.addTopLevelItem(ebook)
-            self.ui.treeWidget.repaint()
+                if fname.endswith("epub") and not os.path.isdir(fname):
+                    FilesScanned = FilesScanned + 1
+                    fullfile = os.path.join(root, fname)
+                    images = 0
+                    images_corrected = 0
+                    ebook.setText(0, fname)
+                    self.ui.statusbar.showMessage("Checking " + fname + "...")
+                    print "Checking " + fname + "..."
+                    try:
+                        test = epub.EpubFile(fullfile)
+                        for item in test.opf.manifest:
+                            if str.startswith(str(item.mimetype), "image"):
+                                images = images+1
+                                img = Image.open(StringIO(item.read()))
+                                if self.testImage(img):
+                                    images_corrected = images_corrected+1
+                    except:
+                        images_corrected = -1        
+                    ebook.setText(2, str(images))
+                    ebook.setText(3, str(images_corrected))
+                    if images_corrected == 0:
+                        ebook.setText(1, "OK")
+                        ebook.setTextColor(1, self.okcolor)
+                    elif images_corrected == -1:
+                        ebook.setText(1, "ERROR")
+                        ebook.setTextColor(1, self.errorcolor)
+                    else:
+                        EpubsAffected = EpubsAffected + 1
+                        ebook.setText(1, "Warnings")
+                        ebook.setTextColor(1, self.warningcolor)
+                        self.filesToFix.append((fullfile, ebook))
+                    epubsize = os.path.getsize(fullfile)
+                    nicesize = self.prettySize(epubsize)
+                    ebook.setText(4, nicesize)
+                    TotalImagesFound = TotalImagesFound + images
+                    TotalImagesFix = TotalImagesFix + images_corrected
+                self.ui.treeWidget.addTopLevelItem(ebook)
+                self.ui.treeWidget.repaint()
             
         endtime = int(time.time())
         timetaken = endtime - starttime
@@ -104,6 +142,10 @@ class Main(QtGui.QMainWindow):
             , "Report"
             , "EPUBs Scanned:\t" + str(FilesScanned) + "\nImages Found:\t\t" + str(TotalImagesFound) + "\nImages Over Limit:\t" + str(TotalImagesFix) + "\nEPUBs Affected:\t" + str(EpubsAffected) + "\n\nTime Taken:\t" + displaytime
             , QtGui.QMessageBox.Ok)
+        
+        self.ui.btnopen.setEnabled(True)
+        self.ui.btnabout.setEnabled(True)
+        self.ui.btnfix.setEnabled(True)
 
     def ConversionDuration(self, seconds):
         seconds = int(seconds)
